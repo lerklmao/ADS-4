86b51966105a47fbe3c7bd14c7d6fb0479395c8e
lerklmao <owins.12KVE@gmail.com>
2026-04-06 22:09:22 +0300

    Королева В.Е. ИСТ-4-1

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,56 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  for (int i = 0; i < len; i++) {
+    for (int j = i + 1; j < len; j++) {
+      if (arr[i] + arr[j] == value) {
+        count++;
+          }
+      }
+  }
+  return count;
 }
-int countPairs2(int *arr, int len, int value) {
-  return 0;
+
+int countPairs2(int* arr, int len, int value) {
+    int count = 0;
+    for (int i = 0; i < len; i++) {
+        for (int j = len - 1; j > i; j--) {
+            if (arr[i] + arr[j] == value) {
+                count++;
+            }
+        }
+    }
+    return count;
 }
-int countPairs3(int *arr, int len, int value) {
-  return 0;
+
+int binarySeeK(int* arr, int left, int right, int target) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target) {
+            int leftmost = mid;
+            int rightmost = mid;
+            while (leftmost > left && arr[leftmost - 1] == target) {
+                leftmost--;
+            }
+            while (rightmost < right && arr[rightmost + 1] == target) {
+                rightmost++;
+            }
+            return rightmost - leftmost + 1;
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return 0;
+}
+
+int countPairs3(int* arr, int len, int value) {
+    int count = 0;
+    for (int i = 0; i < len; i++) {
+        int target = value - arr[i];
+        int cnt = binarySeek(arr, i + 1, len - 1, target);
+        count += cnt;
+    }
+    return count;
 }
